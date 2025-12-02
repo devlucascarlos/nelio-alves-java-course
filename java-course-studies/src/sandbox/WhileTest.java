478bc7e014e45bd81c1ed298a508fd9c56f412c7
Lucas Carlos de Lima Silva <dev.lucascarlos@gmail.com>
2025-12-01 23:24:03 -0300

    test(repetitive): create custom while loop experiment

---
+++ java-course-studies/src/sandbox/WhileTest.java
@@ -0,0 +1,17 @@
+package sandbox;
+
+public class WhileTest {
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = 4;
+
+        while (x < 3){
+            y += 2;
+            x += 1;
+            System.out.println(x + "-" + y);
+        }
+
+        System.out.println("TEST FINISHED");
+    }
+}
