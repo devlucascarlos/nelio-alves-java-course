b9965c6ee9407905eb2635ecd58b94925a4541ed
Lucas Carlos de Lima Silva <dev.lucascarlos@gmail.com>
2025-12-06 22:47:33 -0300

    feat(oop): solve employee salary exercise
    - Implemented Employee entity with net salary calculation logic
    - Added increaseSalary method using percentage logic
    - Created interaction program to read data and update salary

---
+++ java-course-studies/src/oop_fundamentals/workshops/salary/application/Program.java
@@ -0,0 +1,37 @@
+package oop_fundamentals.workshops.salary.application;
+
+import oop_fundamentals.workshops.salary.entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp;
+        emp = new Employee();
+
+        System.out.print("Name: ");
+        emp.name = sc.nextLine();
+        System.out.print("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employee: " + emp);
+
+        System.out.print("Which percentage to increase to salary? ");
+        double percentage = sc.nextDouble();
+
+        emp.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Update data: " + emp);
+
+        sc.close();
+    }
+}
